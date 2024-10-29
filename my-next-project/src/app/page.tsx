47dc7421f5ec1f8cbb80e30c5a4f271a373263d4
crtneko97 <simon_kern@hotmail.com>
2024-10-29 10:01:01 +0100

    productCard fixed and image i had some issues with svg at first changed it back to png but then realized the public folder should be in root...

--- my-next-project/src/app/page.tsx
+++ my-next-project/src/app/page.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Ratings from '@/components/ratings/Ratings';
+import React from 'react';
+import ProductCard from '@/components/productCard/ProductCard';
+import { Product } from '@/types/products/product';
 
 const Home = () => {
+
+//Check if it works
+  const p1: Product = {
+    name: 'Product 1',
+    rating: 4.0,
+    price: 90000,
+    imageUrl: '/images/Product1.png',
+    color: '#DC840A'
+  };
+
   return (
     <div>
-      <Ratings rating={4} />
+      <ProductCard product={p1} />
+    
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- my-next-project/src/components/productCard/ProductCard.tsx
+++ my-next-project/src/components/productCard/ProductCard.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './ProductCard.scss';
+import Ratings from '../ratings/Ratings';
+import { Product } from '@/types/products/product'; 
 
-const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
-  )
+interface ProductCardProps {
+  product: Product;
 }
 
-export default ProductCard
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  return (
+    <div className="product-card">
+
+
+      <div className="product-image">
+      <img
+        src={product.imageUrl}
+        alt={product.name}
+      />
+      </div>
+      
+      <div className="product-info">
+        <h3 className="product-name" style={{ color: product.color }}>
+          {product.name}
+        </h3>
+        
+        <Ratings rating={product.rating} />
+        
+        <div className="price-tag">
+          ${product.price.toLocaleString()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
